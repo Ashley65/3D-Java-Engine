c5a502168c95f0a3b701f85be5e6ff7e59c81c6b
Ashley65 <nchinembiri@outlook.com>
2023-02-28 10:34:23 +0000

    22004221

--- src/main.java
+++ src/main.java
@@ -1,7 +1,9 @@
+import java.awt.*;
 import java.awt.color.*;
 import java.awt.Graphics.*;
 import java.awt.image.*;
 import java.awt.image.BufferedImage;
+
 import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
 import java.util.ArrayList;
@@ -28,6 +30,85 @@ public class Game extends JFrame implements Runnable{
 
     public Game(){
         thread = new Thread(this);
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setTitle("3D engine");
+        setBackground(color.white);
+        start();
+    }
+    public void start(){
+        running = true;
+        thread.start();
+    }
+    public synchronized void stop(){
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void render(){
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
+        for(int i = 0; i < pixels.length; i++){
+            pixels[i] = 0;
+        }
+        for(int i = 0; i < mapWidth; i++){
+            for(int j = 0; j < mapHeight; j++){
+                if(map[i][j] == 1){
+                    for(int k = 0; k < mapScale; k++){
+                        for(int l = 0; l < mapScale; l++){
+                            pixels[(i * mapScale + k) + (j * mapScale + l) * WIDTH] = 0x00ff00;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+    }
+    public void run(){
+        long lastTime = System.nanoTime();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
+        long timer = System.currentTimeMillis();
+        requestFocus();
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println(updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
+        }
     }
 
 
